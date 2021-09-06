a99babe6fdd75818d2bdb20d4cdb19efdb3c9d17
VarnaX <56477171+VarnaX@users.noreply.github.com>
2021-09-06 14:10:51 +0530

    path

--- tortoolkit/functions/ariatools.py
+++ tortoolkit/functions/ariatools.py
@@ -193,7 +193,10 @@ async def aria_dl(incoming_link, c_file_name, sent_message_to_update_tg_p, user_
             file = await aloop.run_in_executor(
                 None, aria_instance.get_download, err_message
             )
-            to_upload_file = file.name
+            savepath = os.path.join(
+                os.getcwd(), "Downloads", str(time.time()).replace(".", "")
+            )
+            to_upload_file = f'{savepath}/{file.name}'
             await ar_task.set_path(to_upload_file)
             await ar_task.set_done()
             return True, ar_task
